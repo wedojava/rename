2c5ae0b8f0f6dab46bb7c586df6eca83fe41a386
wedojava <wedojava@gmail.com>
2020-08-18 19:32:47 +0800

    enable -dir arg, add goreleaser

--- main.go
+++ main.go
@@ -13,11 +13,15 @@ import (
 var dir = flag.String("dir", "./", "where is the files you wanna rename?")
 
 func main() {
+	flag.Parse()
 	filelist, err := ioutil.ReadDir(*dir)
 	if err != nil {
 		log.Println(err)
 	}
 	for _, f := range filelist {
+		if strings.HasSuffix(f.Name(), ".exe") {
+			continue
+		}
 		os.Rename(filepath.Join(*dir, f.Name()),
 			filepath.Join(*dir, match(f.Name())))
 	}
